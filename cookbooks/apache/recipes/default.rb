1ed6d77fc5d44ebffdb64aa14997f3aa965ea32f
Sergio <sergio.garcia@icf.com>
2017-05-12 20:54:07 +0000

    added motd file

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == "rhel"
+  package = 'httpd'
+elsif node['platform_family'] == "debian"
+  package = "apache2"
+end
+
 package 'apache2' do
-  package_name 'httpd'
+  package_name package
   action :install
 
 end
@@ -16,4 +22,4 @@ service 'apache2' do
 
 end
 
-
+#include_recipe 'apache::websites'
